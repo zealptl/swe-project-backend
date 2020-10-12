3a480dcafd890cba0395925e31c75622c2b64751
Greg <gkimatov@gmail.com>
2020-10-12 01:13:15 -0400

    add MenuItems model to develop

---
+++ src/models/MenuItems.ts
@@ -0,0 +1,25 @@
+import { Document, model, Schema } from 'mongoose';
+
+// NOTE/ TODO: For now I have set image as string, but I think we need some extra libraries to upload/ return from DB
+
+export interface MenuItems extends Document {
+  title: String;
+  chefName: String;
+  description: String;
+  ingredients: String[];
+  dietaryRestrictions: String[];
+  image: String;
+  type: String;
+}
+
+const menuItemsSchema = new Schema({
+  title: String,
+  chefName: String,
+  description: String,
+  ingredients: Array,
+  dietaryRestrictions: Array,
+  image: String,
+  type: String,
+});
+
+export default model<MenuItems>('MenuItemsModel', menuItemsSchema);
