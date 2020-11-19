c3c64a2d5a0e328c9a38a3c8bb45257550779c5a
bhavrish <bhavrish@gmail.com>
2020-11-18 23:35:33 -0500

    Add endpoints for creating and getting manager details

--- src/routes/manager.ts
+++ src/routes/manager.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { createManager } from './controllers/ManagerControllers/createManager';
+import { getManager } from './controllers/ManagerControllers/getManager';
 import { approveCustomer } from './controllers/ManagerControllers/approveCustomer';
 
 const router = express.Router();
+router.post('/', createManager);
+router.get('/:managerId', getManager);
 router.post('/approve/:customerId', approveCustomer);
 
 export default router;
