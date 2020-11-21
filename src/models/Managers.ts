1ba0677a6a57e3f5b2ea226a3d3e892a8bbb6867
zealptl <zealpatel06@gmail.com>
2020-11-21 17:15:45 -0500

    refactoring models to only store ids

--- src/models/Managers.ts
+++ src/models/Managers.ts
@@ -1,56 +1,52 @@
-import { Document, model, Mongoose, Schema } from 'mongoose';
-import { Customers } from './Customers';
-import { Employees } from './Employees';
-
-
+import { Document, model, Schema } from 'mongoose';
 
 export interface Managers extends Document {
   firstName: String;
   lastName: String;
   email: String;
   password: String;
-  customerToApprove: Array<Customers>;
-  chefToApprove: Array<Employees>;
-  deliveryPersonToApprove: Array<Employees>;
+  customerToApprove: Array<String>;
+  chefToApprove: Array<String>;
+  deliveryPersonToApprove: Array<String>;
   created_at: Date;
   updated_at: Date;
 }
 
 const managersSchema = new Schema({
-    firstName: {
-        type: String,
-        required: 'Please enter your first name'
-    },
-    lastName: {
-        type: String,
-        required: 'Please enter your last name'
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: 'Please enter your email'
-    },
-    password: {
-        type: String,
-        required: 'Please enter password'
-    },
-    customerToApprove: {
-        type: Array
-    },
-    chefToApprove: {
-        type: Array
-    },
-    deliveryPersonToApprove: {
-        type: Array
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    },
+  firstName: {
+    type: String,
+    required: 'Please enter your first name',
+  },
+  lastName: {
+    type: String,
+    required: 'Please enter your last name',
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: 'Please enter your email',
+  },
+  password: {
+    type: String,
+    required: 'Please enter password',
+  },
+  customerToApprove: {
+    type: [Schema.Types.ObjectId],
+  },
+  chefToApprove: {
+    type: [Schema.Types.ObjectId],
+  },
+  deliveryPersonToApprove: {
+    type: [Schema.Types.ObjectId],
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default model<Managers>('Managers', managersSchema);
