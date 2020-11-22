de0ba919603029f7feff4432d0fc422e2588d2d7
zealptl <zealpatel06@gmail.com>
2020-11-22 12:30:19 -0500

    adding delete employee and customer controller

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -2,10 +2,18 @@ import express from 'express';
 import { createCustomer } from './controllers/CustomerControllers/createCustomer';
 import { getCustomers } from './controllers/CustomerControllers/getCustomers';
 import { getCustomer } from './controllers/CustomerControllers/getCustomer';
+import { deleteCustomer } from './controllers/CustomerControllers/deleteCustomer';
+import { isUserAllowedMiddleware } from './middlewares/isUserAllowed';
+import { isUserSignedInMiddleware } from './middlewares/isUserSignedIn';
 
 const router = express.Router();
 router.post('/', createCustomer);
 router.get('/', getCustomers); // get all customers
 router.get('/:customerId', getCustomer); // get particular customer
-
+router.delete(
+  '/:customerId',
+  isUserSignedInMiddleware,
+  isUserAllowedMiddleware(['manager', 'customer']),
+  deleteCustomer
+);
 export default router;
