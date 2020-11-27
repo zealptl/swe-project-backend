c75afda8bb5a5fd0631e4887c5a434c551c479bd
bhavrish <bhavrish@gmail.com>
2020-11-27 17:59:59 -0500

    Add delete endpoint for discussions

--- src/routes/discussion.ts
+++ src/routes/discussion.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { postToDiscussion } from './controllers/DiscussionController/postToDiscussion';
 import { getDiscussions } from './controllers/DiscussionController/getDiscussions';
 import { updateDiscussion } from './controllers/DiscussionController/updateDiscussion';
+import { deleteDiscussionPost } from './controllers/DiscussionController/deleteDiscussionPost';
 
 
 const router = express.Router();
 router.post('/', postToDiscussion);
 router.get('/', getDiscussions);
 router.patch('/:discussionPostId', updateDiscussion);
+router.delete('/:discussionPostId', deleteDiscussionPost);
 
 export default router;
