6a1a16f9d6f1948d13756d9c2de11ec920d4cde3
Greg <gkimatov@gmail.com>
2020-12-06 18:59:43 -0500

    Update getReviews

--- src/routes/controllers/ReviewsControllers/getReviews.ts
+++ src/routes/controllers/ReviewsControllers/getReviews.ts
@@ -2,9 +2,21 @@ import { Request, Response } from 'express';
 import ReviewsModel, { Reviews } from '../../../models/Reviews';
 
 export const getReviews = async (req: Request, res: Response) => {
-  const reviews: Reviews[] | null = await ReviewsModel.find({});
+  const query: any = {};
+  if (req.query.reviewFrom) 
+    query.reviewFrom = req.query.reviewFrom;
 
-  if (!reviews) res.status(404).json({ msg: 'Reviews not found' });
+  if (req.query.reviewTo) 
+    query.reviewTo = req.query.reviewTo;
+
+  if (req.query.type) 
+    query.type = req.query.type;
+
+  const reviews: Reviews[] | null = await ReviewsModel.find(query);
+
+  if (reviews.length) 
+    res.status(404).json({ msg: 'Reviews not found' });
 
   res.json(reviews);
+  
 };
