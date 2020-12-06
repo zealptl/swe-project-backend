5d7a66d714211f78e6325eba0dc5c2b0d96c07b7
zealptl <zealpatel06@gmail.com>
2020-12-06 09:50:56 -0500

    refactor getMenuItems

--- src/routes/controllers/MenuItemsControllers/getMenuItems.ts
+++ src/routes/controllers/MenuItemsControllers/getMenuItems.ts
@@ -2,9 +2,15 @@ import { Request, Response } from 'express';
 import MenuItemsModel, { MenuItems } from '../../../models/MenuItems';
 
 export const getMenuItems = async (req: Request, res: Response) => {
-  const menuItems: MenuItems[] | null = await MenuItemsModel.find({});
+  const query: any = {};
 
-  if (!menuItems) res.status(404).json({ msg: 'Menu items not found' });
+  if (req.query.chefName) query.chefName = req.query.chefName;
+  if (req.query.type) query.type = req.query.type;
+
+  const menuItems: MenuItems[] | null = await MenuItemsModel.find(query);
+
+  if (menuItems.length === 0)
+    res.status(404).json({ msg: 'Menu items not found' });
 
   res.json(menuItems);
 };
