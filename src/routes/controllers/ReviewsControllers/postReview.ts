f1e48b4b055a40e211f525af52429687918dc035
bhavrish <bhavrish@gmail.com>
2020-12-11 12:21:06 -0500

    Accept from and to types

--- src/routes/controllers/ReviewsControllers/postReview.ts
+++ src/routes/controllers/ReviewsControllers/postReview.ts
@@ -6,7 +6,9 @@ export const postReview = async (req: Request, res: Response) => {
     let review: Reviews = new ReviewsModel({
       review: req.body.review,
       reviewFrom: req.body.reviewFrom,
+      reviewFromType: req.body.reviewFromType,
       reviewTo: req.body.reviewTo,
+      reviewToType: req.body.reviewToType,
       starRating: req.body.starRating,
       type: req.body.type,
     });
